e22ef21a2e5ea9e1bc0448055109712214508c43
Icenowy Zheng <icenowy@aosc.xyz>
2016-10-08 14:22:22 +0800

    try pypy3 is available
    Signed-off-by: Icenowy Zheng <icenowy@aosc.xyz>

--- message_gen/one_sentence.sh
+++ message_gen/one_sentence.sh
@@ -1,1 +1,7 @@
-python3 message_gen/speak.py | sed 's/^> //g' | sed '/^$/d'
+py=python3
+if which pypy3 > /dev/null 2>&1
+then
+	py=pypy3
+fi
+
+$py message_gen/speak.py | sed 's/^> //g' | sed '/^$/d'
